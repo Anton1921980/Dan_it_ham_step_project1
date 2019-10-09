d5860073025f327d6807d49b11d90c36d2997e35
Anton Molchanovskyi <mywear1@gmail.com>
2019-10-10 01:44:55 +0300

    fixes

--- homework10js/script/main.js
+++ homework10js/script/main.js
@@ -1,42 +1,50 @@
-function show() {
-    let p = document.getElementById('pass');
-    p.setAttribute('type', 'text');       
-    event.target.classList.toggle('fa-eye-slash');
-    event.target.classList.toggle('fa-eye');
-        
+let submit = document.querySelector(".btn");
+submit.addEventListener("click", function () {
+    let pass = document.getElementById('pass');
+    let pass2 = document.getElementById('pass2');
+    console.log(pass.value);
+    console.log(pass2.value);
+    if (pass.value === pass2.value && pass.value !== "" && !pass.value.includes(" ")) {
+        document.querySelector(".spn").style.visibility = 'hidden';
+        console.log("welcome");
+        alert("You are welcome");
+    }
+    else {
+        console.log("ввести одинаковые");
+        document.querySelector(".spn").style.visibility = 'visible';
+    }
+});
+
+let showHide = function () {
+    if (this.previousElementSibling.type == 'text') {
+        this.previousElementSibling.setAttribute('type', 'password');
+        event.target.classList.remove('fa-eye');
+        event.target.classList.add('fa-eye-slash');
+        return;
+    }
+    else {
+        this.previousElementSibling.setAttribute('type', 'text');
+        event.target.classList.remove('fa-eye-slash');
+        event.target.classList.add('fa-eye');
+        return;
+    }
 }
 
-function hide() {
-    let p = document.getElementById('pass');
-    p.setAttribute('type', 'password');
-    event.target.classList.toggle('fa-eye-slash');
-    event.target.classList.toggle('fa-eye');
-}
+// document.getElementById("eye").addEventListener("click", showHide, false);
+// document.getElementById("eye2").addEventListener("click", showHide,false);
 
-let passShown = 0;
-// let qqq=document.querySelectorAll("#eye, #eye2");
-// console.log(qqq);
-let rrr=document.getElementById("eye");
-let sss=document.getElementById("eye2");
+document.querySelectorAll('.fas').forEach(function (btn) {
+    btn.addEventListener('click', showHide, false)
+});
 
-rrr.addEventListener("click", function () {
-    if (passShown == 0) {
-        passShown = 1;
-        show();
 
-    } else {
-        passShown = 0;
-        hide();
-    }
-}, false);
 
-sss.addEventListener("click", function () {
-    if (passShown == 0) {
-        passShown = 1;
-        show();
 
-    } else {
-        passShown = 0;
-        hide();
-    }
-}, false);
+
+
+
+
+
+
+
+
