9eaa84626791c53da40eda6032328d0780fbd390
Anton Molchanovskyi <mywear1@gmail.com>
2020-08-16 14:36:37 +0300

    del_homeworks

--- homework6js/main/script.js
+++
@@ -1,7 +0,0 @@
-let arr = ['hello', 'world', 23, '23', null];
-function filterBy(arr, type) {
-    return arr.filter(value => typeof value !== type)
-}
-let sortedArr = filterBy(arr, 'string');
-console.log(sortedArr);
-
